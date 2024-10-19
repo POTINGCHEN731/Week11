f15b1a3ee12101777153f964b08cc50e3182c0ab
POTINGCHEN731 <163748756+POTINGCHEN731@users.noreply.github.com>
2024-10-19 14:11:22 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [vue(), vueDevTools()],
-  base: mode === 'production' ? '/Week11/' : '/', // 根据环境设置 base 路径
+  base: mode === 'production' ? '/Week11/' : '/', 
   resolve: {
     alias: {
       '@': new URL('./src', import.meta.url).pathname
